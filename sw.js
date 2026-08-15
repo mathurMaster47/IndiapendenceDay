2bacafc3947702ead7cfee1aaeb8c278316c6300
mathurMaster47 <itsanshmathur@gmail.com>
2026-08-15 13:32:10 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'indiapendenceday';
+const ASSETS = ['./index.html', './manifest.json', './favicon.jpeg'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
