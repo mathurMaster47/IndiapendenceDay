ec67e1560e79c9e1c7ab0ecb1dcc94b814645c78
mathurMaster47 <itsanshmathur@gmail.com>
2026-08-17 13:43:53 +0530

    Enhance fetch event to cache responses
    Updated fetch event handling to cache fresh responses and fallback to cache when offline.

--- sw.js
+++ sw.js
@@ -6,5 +6,19 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+    e.respondWith(
+        fetch(e.request)
+            .then((response) => {
+                // First update cache with fresh network response
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(e.request, responseClone);
+                });
+                return response;
+            })
+            .catch(() => {
+                // If offline / non-internet, use cache version
+                return caches.match(e.request);
+            })
+    );
 });
